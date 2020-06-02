2d83592d22acaae3d0ebeadaa8a1fb829fed8484
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-06-01 19:15:01 -0700

    setup redux-form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import { reduxForm } from "redux-form";
+
 class SignUp extends Component {
   render() {
     return <div className="signup">Sign Up</div>;
   }
 }
+
+SignUp = reduxForm({
+  form: "SignUp",
+})(SignUp);
 export default SignUp;
