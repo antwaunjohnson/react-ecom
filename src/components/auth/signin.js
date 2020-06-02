92a9e5d321ee1e0b5427daa220ac08502f8ab91c
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-06-01 18:48:44 -0700

    built signin and signup routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  render() {
+    return <div className="signin">Sign In</div>;
+  }
+}
+export default SignIn;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return <div className="signup">Sign Up</div>;
+  }
+}
+export default SignUp;
