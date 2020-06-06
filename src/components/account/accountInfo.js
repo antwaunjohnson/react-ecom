2b97b635a65d6f5fcd08d0cc06009adfe9abc55b
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-06-06 16:28:35 -0700

    built base structure for account components

---
+++ src/components/account/accountInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AccountInformation extends Component {
+  render() {
+    return (
+      <div>
+        <div>account info</div>
+        <div>account info</div>
+      </div>
+    );
+  }
+}
+export default AccountInformation;
