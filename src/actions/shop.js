970cc26ddb1890025b86bf336e60d0b6eca6bab6
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-06-22 21:04:12 -0700

    completed fetchProducts action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,6 +1,7 @@
 import {
   // SET_SHOP_CATEGORIES
   SET_NAVBAR_LINKS,
+  SET_SHOP_PRODUCTS,
 } from "./types";
 
 export function fetchShopCategories() {
@@ -34,6 +35,86 @@ export function fetchShopCategories() {
       {
         _id: 6,
         title: "UML",
+      },
+    ],
+  };
+}
+
+export function fetchShopProducts() {
+  return {
+    type: SET_SHOP_PRODUCTS,
+    payload: [
+      {
+        _id: 0,
+        title: "JavaScript in the Browser",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 1],
+      },
+      {
+        _id: 1,
+        title: "Graph Database",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 6],
+      },
+      {
+        _id: 2,
+        title: "Full Stack Development",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 3, 4],
+      },
+      {
+        _id: 3,
+        title: "JavaScript Development",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 1],
+      },
+      {
+        _id: 4,
+        title: "User Interface Decise",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 5],
+      },
+      {
+        _id: 5,
+        title: "User Experience Design",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 5],
+      },
+      {
+        _id: 6,
+        title: "Advanced Object Oriented Python",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 3],
+      },
+      {
+        _id: 7,
+        title: "Advanced Object Oriented Ruby",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 4],
+      },
+      {
+        _id: 8,
+        title: "Server Configuration Basics",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit ipsum finibus dignissim elementum.",
+        amount: 1.99,
+        belongsTo: [0, 2],
       },
     ],
   };
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -21,9 +21,7 @@ class Shop extends Component {
     this.props.setHeaderLinks(headerLinks);
     this.props.fetchShopCategories();
     // fetch shop products action creator
-    // set the header links
-    // fetch navbar links
-    // set navbar links
+    this.props.fetchShopProducts();
     // filter products with links
   }
   render() {
