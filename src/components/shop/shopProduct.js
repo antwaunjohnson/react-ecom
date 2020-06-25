fc118fa10b6c21974bb7fda264f30651e5c1811b
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-06-24 21:08:56 -0700

    built green pricetag and quanty component, updated product component

---
+++ src/components/shop/shopProduct.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Shop from "./shop";
+import Quantity from "../quantity";
+import GreenPriceTag from "../greenPriceTag";
+
+class ShopProduct extends Component {
+  render() {
+    const { _id, title, description, amount } = this.props;
+    return (
+      <div className="shop-product">
+        <div className="shop-product__title">{title}</div>
+        <div className="shop-product__description">{description}</div>
+        <GreenPriceTag className="shop-product__price" title={amount} />
+        <Quantity className="shop-product__quantity" quantity={1} />
+        <div className="shop-product__add-to-car">Add to Cart</div>
+      </div>
+    );
+  }
+}
+export default ShopProduct;
